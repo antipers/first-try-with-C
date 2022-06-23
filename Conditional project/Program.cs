1663110dc3d0be3a4e6074974dd3f102cb161ba2
antipers <antipers@gmail.com>
2022-06-23 12:09:27 +0300

    created Conditional_Project

---
+++ Conditional project/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Это же МАША!");
+}
+else
+{
+    Console.WriteLine("Добрый день,");
+    Console.WriteLine(username);
+}
