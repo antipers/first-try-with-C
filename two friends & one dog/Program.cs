e6ac1189c6c025fc9c37b91758404df2029351a9
antipers <antipers@gmail.com>
2022-06-24 20:46:49 +0300

    solving task about friends and dog

---
+++ two friends & one dog/Program.cs
@@ -0,0 +1,24 @@
+﻿int count = 0;
+int first_friend_speed = 1;
+int second_friend_speed = 2;
+int dog_speed = 5;
+int distance = 10000;
+int end = 10;
+int friend = 2;
+while (distance > 10)
+{
+    
+    if (friend = 1);
+    {
+        int time = distance/first_friend_speed + dog_speed;
+    }
+    if (friend =2);
+    {
+        
+    }
+
+}
+
+
+  Console.WriteLine("Собака пробежит- ");
+  Console.Write(count," раз между друзьями");
