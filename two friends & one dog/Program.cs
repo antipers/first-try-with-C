724b822ae275da0c192daea09d33a71c7b55d467
antipers <antipers@gmail.com>
2022-06-24 21:49:55 +0300

    task almost solved,bruh

--- two friends & one dog/Program.cs
+++ two friends & one dog/Program.cs
@@ -5,20 +5,24 @@ int dog_speed = 5;
 int distance = 10000;
 int end = 10;
 int friend = 2;
-while (distance > 10)
+while (distance > end)
 {
     
-    if (friend = 1);
+    if (friend == 1)
     {
-        int time = distance/first_friend_speed + dog_speed;
+        int time = distance / (first_friend_speed + dog_speed);
+        distance = distance - ((first_friend_speed + second_friend_speed) * time);
+        friend = 2;
+        count++;
     }
-    if (friend =2);
+    if (friend == 2)
     {
-        
+      int time = distance / (second_friend_speed + dog_speed);
+      distance = distance - ((first_friend_speed + second_friend_speed) * time);
+      friend = 1 ; 
+      count++;
     }
 
 }
-
-
   Console.WriteLine("Собака пробежит- ");
-  Console.Write(count," раз между друзьями");
+  Console.Write(count);
