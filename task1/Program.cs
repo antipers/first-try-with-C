ba54a8c4f95f7df4a84937a667b27974bb790e7a
antipers <antipers@gmail.com>
2022-06-25 14:05:35 +0300

    solved tasks from lesson

---
+++ task1/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = 5;
+int square = 25;
+if (number * number == square)
+{
+    Console.WriteLine("УРА");
+}
+else
+{
+    Console.WriteLine("FAIL");
+}
---
+++ task2/Program.cs
@@ -0,0 +1,41 @@
+﻿int Mon = 1;
+int Tue = 2;
+int Wed = 3;
+int Thu = 4;
+int Fri = 5;
+int Sun = 6;
+int Sut = 7;
+Console.WriteLine("введите число дня недели от 1 до 7");
+int Week_day = Convert.ToInt32(Console.ReadLine());
+if (Week_day == 1)
+{
+    Console.WriteLine("Mon");
+}
+else if (Week_day == 2)
+{
+    Console.WriteLine("Tue");
+}
+else if (Week_day == 3)
+{
+    Console.WriteLine("Wed");
+}
+else if (Week_day == 4)
+{
+    Console.WriteLine("Thu");
+}
+else if (Week_day == 5)
+{
+    Console.WriteLine("Fri");
+}
+else if (Week_day == 6)
+{
+    Console.WriteLine("Sun");
+}
+else if (Week_day == 7)
+{
+    Console.WriteLine("Sut");
+}
+else
+{
+    Console.WriteLine("Введите корректное число");
+}
