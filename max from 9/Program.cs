7a97d45d9e260f9a6abe2738666423e9c48d4bdd
antipers <antipers@gmail.com>
2022-06-26 23:21:58 +0300

    version 3.0 with methods into methods

--- max from 9/Program.cs
+++ max from 9/Program.cs
@@ -1,8 +1,8 @@
 ﻿int Max(int arg1, int arg2, int arg3)
 {
     int result = arg1;
-    if (arg2 > result)  result = arg2;
-    if (arg3 > result)  result = arg3;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
     return result;
 }
 
@@ -16,9 +16,10 @@ int a3 = 13;
 int b3 = 23;
 int c3 = 38;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
 
-int AAA = Max(max1, max2, max3);
+
+int AAA = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
 Console.WriteLine(AAA);
