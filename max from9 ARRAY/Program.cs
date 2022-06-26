6271385f01167cb64f7c8358509b6619732bcf2d
antipers <antipers@gmail.com>
2022-06-26 23:44:39 +0300

    DONE

---
+++ max from9 ARRAY/Program.cs
@@ -0,0 +1,12 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+//              0  1  2  3  4  5  6  7  8
+int[] Array = { 11, 22, 13, 44, 56, 46, 78, 18, 29 };
+
+int AAA = Max(Max(Array[0], Array[1], Array[2]), Max(Array[3], Array[4], Array[5]), Max(Array[6], Array[7], Array[8]));
+Console.WriteLine("максимальное число- " + AAA);
