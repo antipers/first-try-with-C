a764cb9b8c5001ff2b6840e18420a677d7197e1f
antipers <antipers@gmail.com>
2022-06-27 00:07:59 +0300

    done

---
+++ methods and array/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 88, 24, 35, 84, 15, 46, 79, 88, 59 };
+int n = array.Length;
+int find = 88;
+int index = 0;
+while (index < n)
+{
+if (array[index] == find)
+{
+    System.Console.WriteLine(index);
+    //break; - if need only first result
+}
+index++;
+}
