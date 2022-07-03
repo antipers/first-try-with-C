d661e36400c98391d13ff706fbffd466734ce755
antipers <antipers@gmail.com>
2022-07-04 00:22:30 +0300

    completed

---
+++ planes2/Program.cs
@@ -0,0 +1,20 @@
+﻿void printVar(int planesNumber) 
+
+{
+if (planesNumber == 1)
+System.Console.WriteLine("x > 0 and y > 0");
+
+else if (planesNumber == 2)
+System.Console.WriteLine("x < 0 and y > 0");
+
+else if (planesNumber == 3)
+System.Console.WriteLine("x < 0 and y < 0");
+
+else if (planesNumber == 4)
+System.Console.WriteLine("x > 0 and y < 0");
+
+else 
+System.Console.WriteLine("Некорректные входные данные");
+}
+
+printVar(3);
--- task21/Program.cs
+++ task21/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Введите координаты А ");
+﻿/* Console.WriteLine("Введите координаты А ");
 int AX = Convert.ToInt32(Console.ReadLine());
 int AY = Convert.ToInt32(Console.ReadLine());
 
@@ -12,4 +12,14 @@ int dY = AY - BY;
 
 
 Double rangeC = Math.Sqrt(dX * dX + dY * dY);
-System.Console.WriteLine(rangeC);
+System.Console.WriteLine(rangeC); */
+
+void printGipo(double aX, double aY, double bX, double bY)
+{
+    double dX = aX - bX;
+    double dY = aY - bY;
+    double rangeC = Math.Sqrt(dX * dX + dY * dY);
+    System.Console.WriteLine(rangeC);
+}
+
+printGipo(4, 3, 5, 6);
