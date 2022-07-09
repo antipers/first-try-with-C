a5d9baefdfbbf4465ee441127df555950f8f3c59
antipers <antipers@gmail.com>
2022-07-09 15:54:17 +0300

    3/5 tasks has been solved from seminar lesson

---
+++ task32/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2]
+ */
+
+ int[] array = {-4, -8, 8, 2};
+
+//  void PrintArray(int[] array)
+//  {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//       System.Console.Write($"{array[i]} / ");
+//     }
+//  }
+
+ void changeArray(int[] array)
+ {
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = array[i]*-1;
+    System.Console.Write($"{array[i]} / ");
+}
+ }
+changeArray(array);
---
+++ task35/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество двухзначных элементов массива.
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+
+int[] array = new int[123];
+
+void fillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1,1000);
+        System.Console.Write($"{array[i]}  , ");
+    }
+}
+
+
+void findNumbers(int[] array)
+{int count=0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]>9 && array[i]<100)
+        {
+            count++;
+        }
+    }
+System.Console.WriteLine(count);
+}
+
+fillArray(array);
+System.Console.WriteLine("=============================");
+findNumbers(array);
